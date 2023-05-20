40f7750245896b5ce961ab6f12c6832f7f2c5412
diana nica <themindhelp@gmail.com>
2023-05-20 11:03:02 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Newsletter from "./components/Newsletter";
+import Analytics from "./components/Analytics";
 function App() {
-  return;
-  <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+    </div>
+  );
 }
 
 export default App;
